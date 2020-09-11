966d89e6bd35219ab576455fb69ad33687867851
xinshangshangxin <xinshangshangxin@gmail.com>
2020-09-11 14:57:21 +0800

    手动打开浏览器

--- projects/all/yh.ts
+++ projects/all/yh.ts
@@ -1,28 +1,12 @@
 import { boundsClick } from '../common/click-ele-bounds';
-import { floatyDebug } from '../common/floaty-debug';
 import { smlMove } from '../common/swipe';
 import { tl } from '../common/toast';
 
-function capture() {
-  tl('开始寻找 智能验证 按钮');
-  const ele = textContains('点击按钮开始智能验证').findOne()?.parent();
-  if (!ele) {
-    tl('没有找到 智能验证 按钮');
-    return;
-  }
+function capture(ele: UiObject) {
   const rect = ele.bounds();
   boundsClick(rect);
   sleep(5000);
-  floatyDebug(
-    {
-      x: rect.left,
-      y: rect.top + rect.height() / 2,
-    },
-    {
-      x: rect.right,
-      y: rect.top + rect.height() / 2,
-    }
-  );
+
   const move = {
     from: {
       x: rect.left,
@@ -37,13 +21,23 @@ function capture() {
   smlMove(move);
 
   sleep(1000);
-  capture();
 }
 
 function start(): void {
   tl('请手动打开浏览器, 并刷新页面');
+  app.startActivity('console');
   sleep(3000);
-  capture();
+
+  setInterval(() => {
+    tl('开始寻找 智能验证 按钮');
+    const ele = textContains('点击按钮开始智能验证').findOnce()?.parent();
+    if (!ele) {
+      tl('没有找到 智能验证 按钮');
+      return;
+    }
+
+    capture(ele);
+  }, 5000);
 }
 
 export { start };
