93919019a61d5f897dcec8d3eb06ce25bcf248ae
xinshangshangxin <xinshangshangxin@gmail.com>
2022-07-08 14:13:04 +0800

    refactor: rm old

--- projects/suning/fruits/tasks/is-in-package.ts
+++
@@ -1,7 +0,0 @@
-import { suningApplicationId } from '../../../common/app/old-open-app';
-
-function throwWhenNotInPackage(): boolean {
-  return currentPackage() === suningApplicationId;
-}
-
-export { throwWhenNotInPackage };
--- projects/suning/whale/sign-in.ts
+++
@@ -1,5 +0,0 @@
-function signIn(): void {
-  textContains('立即签到').findOnce()?.click();
-}
-
-export { signIn };
